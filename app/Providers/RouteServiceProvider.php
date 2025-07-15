b619e748a943abefd3a83de109d7124d20c97545
Robleh Farah <roblehfarah@Roblehs-MacBook-Air.local>
2025-07-15 11:56:50 +0300

    Fix Providers issue, api sync route now established as expected

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        file_put_contents(base_path('routeprovider.txt'), 'RouteServiceProvider booted at '.date('c').PHP_EOL, FILE_APPEND);
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,7 @@
 use App\Http\Controllers\SyncController;
 
 Route::post('/etsy/sync', [SyncController::class, 'syncCustomers']);
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'Test route works']);
+});
